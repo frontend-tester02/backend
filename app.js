2c99df14bdc932f4f1309dfffb859770b2b6a6a1
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2025-02-25 02:30:31 +0900

    Add routes, server and contollers

--- app.js
+++ app.js
@@ -4,40 +4,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const postModel = require('./models/post.model')
 
+const postRoute = require('./routes/post.route')
 const app = express()
 
 app.use(express.json())
 
-app.get('/', async (req, res) => {
-	try {
-		const allPosts = await postModel.find()
-		res.status(200).json(allPosts)
-	} catch (error) {
-		res.status(500).json(error)
-	}
-})
-
-app.post('/', async (req, res) => {
-	try {
-		const { title, body } = req.body
-		const newPost = await postModel.create({ title, body })
-		res.status(201).json(newPost)
-	} catch (error) {
-		res.status(500).json(error)
-	}
-})
-
-app.delete('/:id', (req, res) => {
-	console.log(req.params)
-	res.send('Hello')
-})
-
-app.put('/:id', (req, res) => {
-	const { id } = req.params
-	const body = req.body
-
-	res.send({ id: id, body: body })
-})
+app.use('/api/post', postRoute)
 
 const PORT = process.env.PORT || 8080
 
