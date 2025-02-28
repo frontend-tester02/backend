3ccafd8b5873be17d124717cb996f7a9d7978256
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2025-03-01 02:01:00 +0900

    Middlewares

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const mongoose = require('mongoose')
 const fileUpload = require('express-fileupload')
 
 const postRoute = require('./routes/post.route')
+const requestTime = require('./middlewares/request-time')
 const app = express()
 
+app.use(requestTime)
 app.use(express.json())
 app.use(express.static('static'))
 app.use(fileUpload({}))
--- models/post.model.js
+++ models/post.model.js
@@ -1,10 +1,13 @@
 const { type } = require('express/lib/response')
 const { Schema, model } = require('mongoose')
 
-const postSchema = new Schema({
-	title: { type: String, required: true },
-	body: { type: String, required: true },
-	picture: { type: String },
-})
+const postSchema = new Schema(
+	{
+		title: { type: String, required: true },
+		body: { type: String, required: true },
+		picture: { type: String },
+	},
+	{ timestamps: true }
+)
 
 module.exports = model('Post', postSchema)
