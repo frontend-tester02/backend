aeaf3cd052d65d54607110519fbfa8948ceb857f
frontend-tester02 <shokhrukhnuriddinov@gmail.com>
2025-03-12 01:14:26 +0900

    Login, Logout and Refresh

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -24,6 +24,45 @@ class AuthController {
 			console.log(error)
 		}
 	}
+
+	async login(req, res, next) {
+		try {
+			const { email, password } = req.body
+			const data = await authService.login(email, password)
+			res.cookie('refreshToken', data.refreshToken, {
+				httpOnly: true,
+				maxAge: 30 * 24 * 60 * 60 * 1000,
+			})
+			return res.json(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	async logout(req, res, next) {
+		try {
+			const { refreshToken } = req.cookies
+			const token = await authService.logout(refreshToken)
+			res.clearCookie('refreshToken')
+			return res.json({ token })
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	async refresh(req, res, next) {
+		try {
+			const { refreshToken } = req.cookies
+			const data = await authService.refresh(refreshToken)
+			res.cookie('refreshToken', data.refreshToken, {
+				httpOnly: true,
+				maxAge: 30 * 24 * 60 * 60 * 1000,
+			})
+			return res.json(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
 }
 
 module.exports = new AuthController()
